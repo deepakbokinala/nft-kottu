eb23ea738cc110a4c7dfe8a6ebdb4b262b8e39c7
Chanukya-hitman <revathipathiyarabati@gmail.com>
2022-10-04 00:57:32 +0530

    Fixed Nav Dropdown

--- components/Header.js
+++ components/Header.js
@@ -57,31 +57,37 @@ const Header = () => {
                 <Link href="/live-auctions">
                     <div className={style.headerItem}>Live Auctions</div>
                 </Link>
-
-                <UncontrolledDropdown>
-                    <DropdownToggle
-                        caret
-                        color="dark"
-                    >
-                        Use-Cases
-                    </DropdownToggle>
-                    <DropdownMenu dark>
-
-                        <DropdownItem>
-                            <Link href="/education">
-                                Education
-                            </Link>
-                        </DropdownItem>
-
-                        <DropdownItem>
-                            <Link href="/realEstate">
-                                Real Estate
-                            </Link>
-                        </DropdownItem>
-
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-
+                <div class="group inline-block">
+  <button
+    class="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
+  >
+    <span class="pr-1 font-semibold flex-1">UseCases </span>
+    <span>
+      <svg
+        class="fill-current h-4 w-4 transform group-hover:-rotate-180
+        transition duration-150 ease-in-out"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+        />
+      </svg>
+    </span>
+  </button>
+  <ul
+    class="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute 
+  transition duration-150 ease-in-out origin-top min-w-32"
+  >
+    <li class="rounded-sm px-3 py-1 hover:bg-gray-100"><Link href="/education">
+                    <div className={style.headerItem}>Education</div>
+                </Link></li>
+    <li class="rounded-sm px-3 py-1 hover:bg-gray-100"><Link href="/realEstate">
+                    <div className={style.headerItem}>Real Estate</div>
+                </Link></li>
+    
+  </ul>
+</div>
 
                 <div className={style.headerItem}>Create</div>
                 <Link href="/about">
